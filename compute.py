95451185599dc780cf5b04e42be6dc329e04c414
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2022-05-18 18:25:22 +0900

    add pytest

--- compute.py
+++ compute.py
@@ -25,13 +25,13 @@ def mymult(a, b):
 
     if a > 0 and b > 0:
         return a * b
-    
+
     if a < 0 and b < 0:
         return a * b
 
     if a > 0 and b < 0:
         return a * b
-    
+
     if a < 0 and b > 0:
         return a * b
 
@@ -42,3 +42,25 @@ def mymult(a, b):
         return 0
 
     return -100
+
+
+def test_myadd():
+
+    assert myadd(1, 2) == 3
+    assert myadd(2, 6) > 0
+    assert myadd(-2, -6) < 0
+    assert myadd(0, 6) == 6
+    assert myadd(2, 0) == 2
+    assert myadd(4, -1) > 0
+    assert myadd(1, -4) < 0
+
+
+def test_mymult():
+
+    assert mymult(2, 6) == 12
+    assert mymult(2, 6) > 0
+    assert mymult(-2, -6) > 0
+    assert mymult(2, -6) < 0
+    assert mymult(-2, 6) < 0
+    assert mymult(0, 6) == 0
+    assert mymult(2, 0) == 0
